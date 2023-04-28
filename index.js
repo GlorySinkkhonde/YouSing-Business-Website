96dbb8cacfd123ab107bd1df4152a892095e3407
Glory Sinkhonde <sinkhondeglory@gmail.com>
2023-04-28 20:28:44 +0200

    Completed the mobile nav

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const navToggle = document.querySelector('.navigation__toggle');
 const primaryNav = document.querySelector('.navigation__primary');
-const navLogo = document.querySelector('.navigation__logo');
 
 
 navToggle.addEventListener('click', () => {
