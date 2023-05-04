1996e8858f58ce345323479fab7878bb77b49089
Glory Sinkhonde <sinkhondeglory@gmail.com>
2023-05-04 08:12:22 +0200

    Added a slider

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const navToggle = document.querySelector('.navigation__toggle');
 const primaryNav = document.querySelector('.navigation__primary');
+const slideForwardBtn= document.querySelector('.slide-forward')
+const slideBackBtn= document.querySelector('.slide-back')
+const sliderOne = document.querySelector('.testimonial__slider-container-1')
+const sliderTwo = document.querySelector('.testimonial__slider-container-2')
+const test = document.querySelector('.testimonials')
 
 
 navToggle.addEventListener('click', () => {
     primaryNav.classList.toggle('nav--visible');
 })
+
+slideForwardBtn.addEventListener('click', () => {
+    sliderOne.classList.toggle('disappear')
+    sliderTwo.classList.toggle('appear')
+})
+
+slideBackBtn.addEventListener('click', () => {
+    test.style.backgroundColor = 'red'
+})
+
