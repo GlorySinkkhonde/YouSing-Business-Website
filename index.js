706d8fe6b9df8c95b8952460e5181a29571f174d
Glory Sinkhonde <sinkhondeglory@gmail.com>
2023-05-11 13:33:49 +0200

    Finished slider and footer

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const navToggle = document.querySelector('.navigation__toggle');
 const primaryNav = document.querySelector('.navigation__primary');
-const slideForwardBtn= document.querySelector('.slide--forward')
-const slideBackBtn= document.querySelector('.slide--back')
-const sliderOne = document.querySelector('.testimonials__container-one')
-const sliderTwo = document.querySelector('.testimonials__container-two')
-const test = document.querySelector('.testimonials')
+const slideForwardBtn = document.querySelector('.slide--forward')
+const slideBackBtn = document.querySelector('.slide--back')
+const testimonialContainer = [...document.querySelectorAll('.testimonials__container')]
 
 
 navToggle.addEventListener('click', () => {
     primaryNav.classList.toggle('nav--visible');
 })
 
-slideForwardBtn.addEventListener('click', () => {
-    sliderOne.classList.toggle('disappear')
-    sliderTwo.classList.toggle('appear')
+testimonialContainer.forEach((item, i) => {
 
-})
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
-slideBackBtn.addEventListener('click', () => {
+    slideForwardBtn.addEventListener('click', () => {
+        console.log(containerWidth)
+        item.scrollLeft += containerWidth;
+    })
     
+    slideBackBtn.addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+
 })
+
+
 
